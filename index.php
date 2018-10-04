6ac2582430551089f7bf02fe1d025377133d4ee9
Tiago Wesguerber <43017905+stratotw@users.noreply.github.com>
2018-10-03 22:01:36 -0300

    Aula 1
    Aprendendo um pouco sobre HTML e introdução ao PHP

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>Curso de PHP</title>
+		<meta charset="UTF-8"> <!-- Pacote de caracteres especiais -->
+	</head>
+	<body>
+		<?php
+			$i = 0;
+				while ($i++ < 10) {
+				echo "1 * ".$i." = ".($i*1)."<br>";
+				echo "2 * ".$i." = ".($i*2)."<br>";
+				echo "3 * ".$i." = ".($i*3)."<br>";
+				echo "4	* ".$i." = ".($i*4)."<br>";
+				echo "5 * ".$i." = ".($i*5)."<br>";
+				echo "6 * ".$i." = ".($i*6)."<br>";
+				echo "7 * ".$i." = ".($i*7)."<br>";
+				echo "8 * ".$i." = ".($i*8)."<br>";
+				echo "9 * ".$i." = ".($i*9)."<br>";
+				echo "10 * ".$i." = ".($i*10)."<br>";
+				echo "<br>";
+				}
+		?>		
+	</body>
+</html>
